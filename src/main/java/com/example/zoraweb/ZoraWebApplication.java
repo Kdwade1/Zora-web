79456f64d570277107ebcef763e9cffa2d889cbc
Kelvin <kelvin.daquon.wade@gmail.com>
2022-09-12 16:01:19 -0500

    update

---
+++ src/main/java/com/example/zoraweb/ZoraWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoraweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoraWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoraWebApplication.class, args);
+    }
+
+}
