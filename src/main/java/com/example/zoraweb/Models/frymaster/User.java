327c6fb60e0505c377a6bda687cefdf87e49e194
Kelvin <kelvin.daquon.wade@gmail.com>
2022-09-19 14:03:09 -0500

    fixing page

--- src/main/java/com/example/zoraweb/Models/frymaster/User.java
+++ src/main/java/com/example/zoraweb/Models/frymaster/User.java
@@ -22,25 +22,43 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @Column(nullable= false)
+    private long zipcode;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable=false)
+    private String state;
 
 
-    public User(long id, String username, String password, String profile_img, String email) {
+    public User(User user) {
+    }
+
+    public User(long id, String username, String password, String profile_img, String email, long zipcode, String address, String city, String state) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.profile_img = profile_img;
         this.email = email;
+        this.zipcode = zipcode;
+        this.address = address;
+        this.city = city;
+        this.state = state;
     }
 
-    public User(String username, String password, String profile_img, String email) {
+    public User(String username, String password, String profile_img, String email, long zipcode, String address, String city, String state) {
         this.username = username;
         this.password = password;
         this.profile_img = profile_img;
         this.email = email;
-    }
-
-    public User(User user) {
-
+        this.zipcode = zipcode;
+        this.address = address;
+        this.city = city;
+        this.state = state;
     }
 
     public User() {
@@ -84,5 +102,41 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public long getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(long zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
